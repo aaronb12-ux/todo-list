23ed70a1266e9b0947e904daaad709faae815b85
aaronb12 <aaronb@gmail.com>
2025-01-24 18:48:43 -0800

    Added deletion and enhanced the UI.

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import AddTaskButton from "./components/AddTaskButton.jsx";
 import AddEntry from "./components/AddEntry.jsx";
 import FilterButton from "./components/FilterButton.jsx";
+import ShowTasks from "./components/ShowTasks.jsx";
 
 function App() {
 
     const [isformVisible, setIsformVisible] = useState(false);
-    const [todos, setTodos] = useState([])
+    const [tasks, setTasks] = useState([])
+
+
+
+    
 
     return (
         <div className="bg-slate-100 h-screen">
@@ -14,16 +19,23 @@ function App() {
                <span className="mt-2.5 font-extrabold text-3xl text-gray-500">TODO LIST</span>
             </h1>
 
-            <div>
-            <AddTaskButton
-            setIsformVisible={setIsformVisible}
-            />
+            <div className="flex justify-center items-center">
+                <AddTaskButton
+                setIsformVisible={setIsformVisible}
+                />
                 <FilterButton
-                    />
+                />
             </div>
 
             <AddEntry
-                />
+                isformVisible={isformVisible}
+                tasks={tasks}
+                setTasks={setTasks}
+            />
+
+            <ShowTasks
+                tasks={tasks}
+            />
         </div>
 
     )
