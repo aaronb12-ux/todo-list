407679c58f78eaa0f7cc2b01aad2ad13ff82c4fe
aaronb12 <aaronb@gmail.com>
2025-02-02 18:29:47 -0800

    done with frontend! now ending in backend

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -8,38 +8,55 @@ function FilterButton({tasks, setTasks})
 
     const sortedTasksDate = () => {
         setTasks([...tasks].sort((a, b) => b.id - a.id));
+        setShowdropdown(false);
 
     }
     const sortedTasksPriority = () => {
         setTasks([...tasks].sort((a, b) => b.priority_number - a.priority_number));
+        setShowdropdown(false);
     }
 
-    const toggleDropDown = () => {
-
-        let dropdownButton = document.querySelector('#dropdownButton #dropdown');
-        dropdownButton.classList.toggle('hidden');
+    const ShowDropDown = () => {
+        if (showdropdown === true) {
+            setShowdropdown(false);
+            return;
+        }
+        setShowdropdown(true);
     }
-
 
 
     return (
 
         <div>
-            <div className="position: relatvie"  id="dropdownButton"  onClick={() => toggleDropDown()}>
-                <div
-                     className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-semibold rounded-lg shadow-lg hover:shadow-2xl transform hover:-translate-y-0.5 transition duration-400 pl-3 pr-3 pt-2 pb-2 w-24 flex justify-between"
-                    >
+
+                <button
+                    onClick={() => {ShowDropDown()}}
+                     className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-semibold rounded-lg shadow-lg hover:shadow-2xl pl-3 pr-3 pt-2 pb-2 w-24 flex justify-between" >
                     All
-                    </div>
+                    <p>
+                        ▼
+                    </p>
 
-                    <div className="postition: absolute bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-semibold rounded-lg shadow-lg hover:shadow-2xl pl-2 pr-2"
-                         id="dropdown">
-                        <div className="cursor-pointer" onClick={() => sortedTasksDate()}> <span className="text-xs">Most Recent</span> </div>
-                        <div className="cursor-pointer" onClick={() => sortedTasksPriority()}> <span className="text-xs">Priority</span></div>
+                </button>
 
-                    </div>
+            {showdropdown === true ?
+                <div className="absolute  bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-semibold rounded-lg shadow-lg hover:shadow-2xl w-24">
+                <button
+                    onClick={() => {sortedTasksDate()}}
+                    className="block ml-2">
+                    Date
+                </button>
+                <button
+                    onClick={() => {sortedTasksPriority()}}
+                    className="ml-2"
+                >
+                    Priority
+                </button>
+                </div>
+                : null
 
-            </div>
+            }
+
         </div>
     )
 }
@@ -47,4 +64,3 @@ function FilterButton({tasks, setTasks})
 export default FilterButton
 
 
-//bg-gray-400 text-white pl-3 pr-3 pt-2 pb-2 rounded font-bold w-24 flex justify-between"
